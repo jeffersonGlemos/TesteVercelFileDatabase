b3374ae1f4cde77c8f62d5eb2b68bc4a73b3f7fd
jefferson.lemos <jefferson@datweb.com.br>
2020-11-16 13:24:03 -0400

    Testes, acho q está errado

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 module.exports = (req, res) => {
+    const low = require('lowdb')
+    const FileSync = require('lowdb/adapters/FileSync')
     const adapter = new FileSync('db.json')
+    const db = low(adapter)
     db.read()
         .then(() => console.log('Content of my_project/db.json is loaded'))
     name = db.get('posts')
         .map('title')
         .value()
+    console.log(name)
     res.send(name)
 }
